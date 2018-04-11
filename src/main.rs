4ef956af03ea1d24445ba49d79b52f3a38a6fbcb
sashaweiss <sasha@sashaweiss.coffee>
2018-04-10 20:40:58 -0500

    Termion examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate termion;
+
+use termion::{color, cursor, style};
+
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    print!("{}", cursor::Show);
+    // println!("{}Red", color::Fg(color::Red));
+    // println!("{}Blue", color::Fg(color::Blue));
+    // println!("{}Blue'n'Bold{}", style::Bold, style::Reset);
+    // println!("{}Just plain italic", style::Italic);
+
+    print!(
+        "{}{}Stuff",
+        termion::clear::All,
+        termion::cursor::Goto(10, 10)
+    );
+
+    io::stdout().flush().unwrap();
+
+    // loop {}
 }
