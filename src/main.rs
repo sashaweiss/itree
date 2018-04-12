0dafcdb0894f3d05df73ded4c49996d469995025
sashaweiss <sasha@sashaweiss.coffee>
2018-04-12 16:02:19 -0500

    Factor out getting files at root

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ mod cursor;
 mod border;
 
 use std::fs;
+use std::io;
 use std::cmp::Ordering;
 
 fn main() {
-    let _des = files_in_dir(&"./".to_string());
+    let _des = files_in_root();
 
     let mut cur = cursor::new_cursor_bound_to_term();
 
@@ -22,9 +23,12 @@ fn main() {
     cur.interact();
 }
 
-fn files_in_dir(dir: &str) -> Vec<fs::DirEntry> {
-    let mut des: Vec<fs::DirEntry> = fs::read_dir(dir)
-        .unwrap()
+fn files_in_root() -> Vec<fs::DirEntry> {
+    files_in_dir(&"./".to_string()).unwrap()
+}
+
+fn files_in_dir(dir: &str) -> io::Result<Vec<fs::DirEntry>> {
+    let mut des: Vec<fs::DirEntry> = fs::read_dir(dir)?
         .filter(|de| de.is_ok())
         .map(|de| de.unwrap())
         .collect();
@@ -33,5 +37,5 @@ fn files_in_dir(dir: &str) -> Vec<fs::DirEntry> {
         f.path().cmp(&s.path())
     });
 
-    des
+    Ok(des)
 }
